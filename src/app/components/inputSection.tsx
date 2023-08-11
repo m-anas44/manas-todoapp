a6d3ef8d8248400abafe52591b04e66d553835e0
m-anas44 <125805038+m-anas44@users.noreply.github.com>
2023-08-11 19:37:44 +0500

    new update

--- src/app/components/inputSection.tsx
+++ src/app/components/inputSection.tsx
@@ -3,19 +3,15 @@ import { HiOutlineViewGridAdd } from "react-icons/hi"
 const InputSection = () => {
     return (
         <div>
-            <div className='w-full md:max-w-4xl bg-slate-100 mx-auto p-5'>
-                <h1 className='font-bold text-[32px]'>TODO APP</h1>
-                {/* Input Section starts here */}
-                <div className='container flex gap-2 flex-wrap'>
-                    <input type="text" placeholder='Enter Movie Name' className='p-3 pl-5 rounded-full flex-grow-[2] focus:outline-none' />
-                    
-                    <input type="text" placeholder='Enter ID' className=' p-3 pl-5 rounded-full flex-grow-[1] focus:outline-none' />
-                    <button type='button' className='flex-grow-[1] flex items-center gap-x-2 p-3 bg-cyan-700 rounded-full justify-center text-white'>
-                        <HiOutlineViewGridAdd  className="text-xl" />
-                        Add Movie
-                    </button>
-                
-                </div>
+            <h1 className='font-bold text-[32px]'>TODO APP</h1>
+            {/* Input Section starts here */}
+            <div className='container flex gap-2 flex-wrap mt-3'>
+                <input type="text" placeholder='Enter Movie Name' className='p-3 pl-5 rounded-full flex-grow-[2] focus:outline-none' />
+                <input type="text" placeholder='Enter ID' className=' p-3 pl-5 rounded-full flex-grow-[1] focus:outline-none' />
+                <button type='button' className='flex-grow-[1] flex items-center gap-x-2 p-3 bg-cyan-700 rounded-full justify-center text-white'>
+                    <HiOutlineViewGridAdd className="text-xl" />
+                    Add Movie
+                </button>
             </div>
         </div>
     )
