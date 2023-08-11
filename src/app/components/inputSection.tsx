27664ab8f90c4d3e4981adffc65c6fedd22cd2aa
m-anas44 <125805038+m-anas44@users.noreply.github.com>
2023-08-11 20:38:13 +0500

    new

--- src/app/components/inputSection.tsx
+++ src/app/components/inputSection.tsx
@@ -1,19 +1,64 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import { HiOutlineViewGridAdd } from "react-icons/hi"
+import { ImCross } from 'react-icons/im';
 const InputSection = () => {
+    const [inputVal, setInputVal] = useState("");
+    const [id, setId] = useState("");
+    const [todos, setTodos] = useState([
+        { movie: "Extraction", id: 1 },
+        { movie: "Now You See Me", id: 2 }
+    ])
+
+    const addItem = () => {
+        setTodos([...todos,{movie: inputVal, id: id}])
+    }
     return (
         <div>
             <h1 className='font-bold text-[32px]'>TODO APP</h1>
             {/* Input Section starts here */}
             <div className='container flex gap-2 flex-wrap mt-3'>
-                <input type="text" placeholder='Enter Movie Name' className='p-3 pl-5 rounded-full flex-grow-[2] focus:outline-none' />
-                <input type="text" placeholder='Enter ID' className=' p-3 pl-5 rounded-full flex-grow-[1] focus:outline-none' />
-                <button type='button' className='flex-grow-[1] flex items-center gap-x-2 p-3 bg-cyan-700 rounded-full justify-center text-white'>
+                <input type="text" 
+                value={inputVal}
+                placeholder='Enter Movie Name' 
+                onChange={(e)=> setInputVal(e.target.value)}
+                className='p-3 pl-5 rounded-full flex-grow-[2] focus:outline-none' />
+                <input type="number" 
+                placeholder='Enter ID'
+                value={id} 
+                onChange={(e:any)=> setId(e.target.value)}
+                className=' p-3 pl-5 rounded-full flex-grow-[1] focus:outline-none' />
+                <button type='button' 
+                onClick={addItem}
+                className='flex-grow-[1] flex items-center gap-x-2 p-3 bg-cyan-700 rounded-full justify-center text-white'>
                     <HiOutlineViewGridAdd className="text-xl" />
                     Add Movie
                 </button>
             </div>
+
+
+            <div className='container grid grid-cols-1 md:grid-cols-2 gap-3 mt-5'>
+            {
+                todos.map((item: any, index: any) => {
+                    return (
+                            <div className='bg-cyan-700 rounded-md p-2' key={index}>
+                                <div className='flex justify-between'>
+                                    <span className='text-blue-100 text-lg font-bold'>{index+1}</span>
+                                    <span className='text-red-500 font-bold my-auto'>
+                                        <ImCross />
+                                    </span>
+                                </div>
+                                <h2 className='text-xl mt-3 text-slate-100 font-bold'>{item.movie}</h2>
+                                <p className='text-right text-white font-mono underline font-bold'>Edit</p>
+                            </div>
+                    )
+                })
+            }
+                        </div>
         </div>
+
+
+
     )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 import InputSection from "./components/inputSection";
-import MoviesList from "./components/moviesList";
-
 export default function Home() {
   return (
     <div className="w-full md:max-w-4xl bg-slate-100 mx-auto p-5">
       <InputSection />
-      <MoviesList />
     </div>
   )
 }
